b1fb266dcefe16b8a9aeee339c98d9e45fde90e5
marchmello1 <89778018+marchmello1@users.noreply.github.com>
2025-02-22 02:51:43 +0530

    Update __init__.py

--- templates/__init__.py
+++ templates/__init__.py
@@ -1,7 +1,21 @@
 from .job_template import JOB_TEMPLATE
-from .prompt_template import JOB_DESCRIPTION_PROMPT
+from .prompt_template import JOB_DESCRIPTION_PROMPT, QUESTIONS_GENERATION_PROMPT
+from .question_template import (
+    generate_question_set,
+    create_education_question_set,
+    create_work_experience_question_set,
+    create_self_identification_questions
+)
 
 __all__ = [
+    
     'JOB_TEMPLATE',
-    'JOB_DESCRIPTION_PROMPT'
+]
+    'JOB_DESCRIPTION_PROMPT',
+    'QUESTIONS_GENERATION_PROMPT',
+   
+    'generate_question_set',
+    'create_education_question_set',
+    'create_work_experience_question_set',
+    'create_self_identification_questions'
 ]
